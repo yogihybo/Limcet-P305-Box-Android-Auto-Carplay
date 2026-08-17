06db2a3eba7a2384f429ffda8a59f1d822985cd9
yogihybo <yogihybo@users.noreply.github.com>
2026-08-17 22:39:33 +1000

    Add fonts header for staging UI

---
+++ custom_ui/staging_ui/fonts.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "lvgl.h"
+
+// Google Material / Android Auto System Fonts
+LV_FONT_DECLARE(lv_font_roboto_14);
+LV_FONT_DECLARE(lv_font_roboto_20);
+LV_FONT_DECLARE(lv_font_roboto_24);
